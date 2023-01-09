126949a81c0e2b10bf5c276bb5c1a854c4d96ef9
Jacob Hagberg <58997374+JacobHagberg@users.noreply.github.com>
2023-01-08 19:11:55 -0600

    Probably has errors, but ok

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -11,6 +11,13 @@ public class Arm extends SubsystemBase {
     private final CANSparkMax mMotorArm = new CANSparkMax(Constants.ARM, CANSparkMax.MotorType.kBrushless);
     private final CANSparkMax mMotorElevator = new CANSparkMax(Constants.ELEVATOR, CANSparkMax.MotorType.kBrushless);
 
+    RelativeEncoder encoderArm = mMotorArm.getEncoder();
+    SparkMaxPIDController pidArm = mMotorArm.getPIDController();
+    RelativeEncoder encoderElevator = mMotorElevator.getEncoder();
+    SparkMaxPIDController pidElevator = mMotorElevator.getPIDController();
+
+    double Angle, Length;
+
     public Arm(){
         mMotorArm.restoreFactoryDefaults();
         mMotorArm.setIdleMode(IdleMode.kBrake);
@@ -25,6 +32,42 @@ public class Arm extends SubsystemBase {
         mMotorElevator.burnFlash();
     }
 
+    public void SetLengthAndAngle(double angle, double length){
+
+        if (angle < Constants.MinAngle){
+            angle = Constants.MinAngle
+        }
+        if (angle > Constants.MaxAngle){
+            angle = Constants.MaxAngle
+        }
+        if (length < Constants.MinLength){
+            length = Constants.MinLength
+        }
+        if (length > Constants.MaxLength){
+            lenght = Constants.MaxLength
+        }
+
+        Max_Length = (46 / MATH.abs(MATH.cos(angle)))//make shure that this is degrees and not radians
+        Max_Length2 = (66/ MATH.abs(MATH.sin(angle)))
+
+        if length > Max_Length || length > Max_Length2{
+        
+        if Max_Length >= Max_Length2{
+            HardSetPosition(angle, Max_Length2)
+        }else{
+            HardSetPosition(angle, Max_Length)
+        }
+        }
+    }
+
+    private void HardSetPosition(double angle, double length){
+        this.Angle = angle
+        this.Length = lenght
+
+        pidArm.setReference(Angle, CANSparkMax.ControlType.kPosition);
+        pidElevator.setReference(Length, CANSparkMax.ControlType.kPosition);
+    }
+
     public void setPercentOutput(double output) {
         if (output > 1.0) {
             output = 1.0;
@@ -34,5 +77,11 @@ public class Arm extends SubsystemBase {
 
         mMotorArm.set(output);
     }
-}
 
+    public double GetAngle(){
+        return Angle
+    }
+    public double GetLength(){
+        return Length
+    }
+}
